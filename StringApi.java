b2447ae6dfdb7319edc8bcf0ac219ab444f76084
Le Huy <lehuy20@gmail.com>
2012-01-14 17:45:08 +0100

    use import static in java

--- StringApi.java
+++ StringApi.java
@@ -1,25 +1,27 @@
+import static java.lang.System.*;
+
 public class StringApi {
 	public static void main(String[] args){
 		String s3 = "hello" + " world";
-		System.out.println("--- first 5 characters of '" + s3 + "' is '" + s3.substring(0,5) + "'" );
-		System.out.println("--- last 5 characters of '" + s3 + "' is '" + 
+		out.println("--- first 5 characters of '" + s3 + "' is '" + s3.substring(0,5) + "'" );
+		out.println("--- last 5 characters of '" + s3 + "' is '" + 
 			s3.substring(s3.length()-5,s3.length())+"'" );
-		System.out.println("--- position of 'world' in '" + s3 + "' is " +
+		out.println("--- position of 'world' in '" + s3 + "' is " +
 			s3.indexOf("world") );
-		System.out.println("--- replace 'world' by 'moon' in '" + s3 + "' is " +
+		out.println("--- replace 'world' by 'moon' in '" + s3 + "' is " +
 			s3.replaceFirst("world","moon") );
-		System.out.println("--- replace all 'l' by '?' in '" + s3 + "' is " +
+		out.println("--- replace all 'l' by '?' in '" + s3 + "' is " +
 			s3.replaceAll("l","?") );
-		System.out.println("--- split '" + s3 + "' by space ");
-		System.out.print("[");
+		out.println("--- split '" + s3 + "' by space ");
+		out.print("[");
 		String[] arr=s3.split(" ");
 		for(String s: arr){
-			System.out.print(s);
+			out.print(s);
 			if( s != arr[arr.length-1] )
 				System.out.print(",");
 		}
-		System.out.println("]");
-		System.out.println("--- reverse of '" + s3 + "' is  " + 
+		out.println("]");
+		out.println("--- reverse of '" + s3 + "' is  " + 
 			new String(new StringBuffer(s3).reverse()));
 	}
 }
